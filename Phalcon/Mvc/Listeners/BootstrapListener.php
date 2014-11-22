f7e3997361d0288459238c374cb7569036e30b6d
SOFTBOY <cntysoft@gmail.com>
2014-11-22 15:12:50 +0800

    改变标签名称空间路径和路由监听

--- Phalcon/Mvc/Listeners/BootstrapListener.php
+++ Phalcon/Mvc/Listeners/BootstrapListener.php
@@ -80,12 +80,6 @@ class BootstrapListener implements ListenerAggregateInterface
        * 后期有程序来管理这些
        */
       $cfg = ConfigProxy::getGlobalConfig();
-      $router->add('/'.$cfg->platformEntryPoint, array(
-         'module'     => 'Sys',
-         'controller' => 'Index',
-         'action'     => 'platform'
-      ));
-
       $this->configRouter($router, $cfg);
       $router->setDI($di);
       $di->setShared('router', $router);
