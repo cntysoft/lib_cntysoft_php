09fac8d7f070a649317f6916720671f83fdd1dfd
SOFTBOY <cntysoft@163.com>
2015-03-16 22:45:01 +0800

    改进函数原型设置

--- Framework/Qs/Engine/Tag/FuncInvoker.php
+++ Framework/Qs/Engine/Tag/FuncInvoker.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Cntysoft Cloud Software Team
- * 
+ *
  * @author SOFTBOY <cntysoft@163.com>
  * @copyright  Copyright (c) 2010-2011 Cntysoft Technologies China Inc. <http://www.cntysoft.com>
  * @license    http://www.cntysoft.com/license/new-bsd     New BSD License
@@ -13,37 +13,39 @@ use Cntysoft\Framework\Qs\Engine\Tag\Exception;
  */
 class FuncInvoker extends AbstractTag
 {
-    /**
-     * @param string $type 函数类型
-     * @param string $name 函数名称
-     * @param array $params 调用参数
-     */
-    public function call($type,$name, array $params = array())
-    {
-        try{
-            $params['type'] = $type;
-            $this->invokeParams = &$params;
-            $cls = '\\Qs\\Lib\\'.$type;
-            if(!class_exists($cls)){
-                throw new Exception(' Sys function class '.$cls.' is not exist');
-            }
-            if(!method_exists($cls, $name)){
-                throw new Exception('method '.$name.' is not exist');
-            }
-            echo call_user_func_array($cls.'::'.$name, array_slice($params, 0, -1));
-        } catch (\Exception $ex) {
-            echo $this->renderError($ex->getMessage());
-        }
-    }
+   protected $name = null;
+   /**
+    * @param string $type 函数类型
+    * @param string $name 函数名称
+    * @param array $params 调用参数
+    */
+   public function call($type,$name, array $params = array())
+   {
+      try{
+         $params['type'] = $type;
+         $this->invokeParams = &$params;
+         $this->name = $name;
+         $cls = '\\Qs\\Lib\\'.$type;
+         if(!class_exists($cls)){
+            throw new Exception(' Sys function class '.$cls.' is not exist');
+         }
+         if(!method_exists($cls, $name)){
+            throw new Exception('method '.$name.' is not exist');
+         }
+         echo call_user_func_array($cls.'::'.$name, array_slice($params, 0, -1));
+      } catch (\Exception $ex) {
+         echo $this->renderError($ex->getMessage());
+      }
+   }
 
-    /**
-     * @inheritdoc
-     */
-    protected function getTagSignature()
-    {
-        $type = $this->invokeParams['type'];
-        unset($this->invokeParams['type']);
-        return sprintf('Qs::%s(%s)', $type, implode(',', $this->invokeParams));
-    }
+   /**
+    * @inheritdoc
+    */
+   protected function getTagSignature()
+   {
+      $type = $this->invokeParams['type'];
+      unset($this->invokeParams['type']);
+      return sprintf('Qs::%s("%s", "%s")', $type, $this->name, implode('" ,"', $this->invokeParams[0]));
+   }
 
 }
