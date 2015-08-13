71264bd4486325911db016d04a2631f57a3eebb6
SOFTBOY <softboy@fhzc.com>
2015-08-13 14:42:51 +0800

    实现开放搜索查询接口

--- Framework/Cloud/Ali/OpenSearch/Search.php
+++ Framework/Cloud/Ali/OpenSearch/Search.php
@@ -36,6 +36,7 @@ class Search
 
    const D_START = 0;
    const D_HITS = 20;
+   const STATUS_FAIL = 'FAIL';
    /**
     * @var \Cntysoft\Framework\Cloud\Ali\OpenSearch\ApiCaller $apiCaller
     */
@@ -124,7 +125,17 @@ class Search
    public function search(array $opts = array())
    {
       $this->extraOpts($opts);
-      return $this->call(self::QUERY_TYPE_SEARCH);
+      $ret = $this->call(self::QUERY_TYPE_SEARCH);
+      if($ret['status'] == self::STATUS_FAIL){
+         $msg = '';
+         foreach ($ret['errors'] as $error){
+            $msg .= $error['message'];
+         }
+         $errorType = ErrorType::getInstance();
+         Kernel\throw_exception(new Exception(
+            $errorType->msg('QUERY_ERROR', $msg),$errorType->code('QUERY_ERROR')));
+      }
+      return $ret;
    }
 
    /**
