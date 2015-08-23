549b8b8e0619ba7207c711b1ca0c8dc88f141935
SOFTBOY <softboy@fhzc.com>
2015-08-23 14:11:01 +0800

    删除几个不要的函数

--- Kernel/Funcs/Common.php
+++ Kernel/Funcs/Common.php
@@ -418,16 +418,6 @@ function get_server_env($key)
 }
 
 /**
- * 获取一个简便的版本字符串
- * 
- * @return string
- */
-function get_sys_version_str()
-{
-    return ZHUCHAO_VERSION;
-}
-
-/**
  * 移除xss跨站攻击代码
  * 
  * @param string $val
