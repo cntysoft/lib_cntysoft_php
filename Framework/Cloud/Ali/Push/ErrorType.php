5eae0988addcd18c9507b55ccbba83280f07c0ae
wql <wql1211608804@163.com>
2016-03-16 09:14:23 +0800

    百川推送接口

---
+++ Framework/Cloud/Ali/Push/ErrorType.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Cntysoft Cloud Software Team
+ */
+namespace Cntysoft\Framework\Cloud\Ali\Push;
+use Cntysoft\Stdlib\ErrorType as BaseErrorType;
+/**
+ * @author SOFTBOY <cntysoft@163.com>
+ * @copyright  Copyright (c) 2010-2011 Cntysoft Technologies China Inc. <http://www.cntysoft.com>
+ * @license    http://www.cntysoft.com/license/new-bsd     New BSD License
+ */
+class ErrorType extends BaseErrorType
+{
+   protected $map = array(
+   );
+
+}
---
+++ Framework/Cloud/Ali/Push/ResultSet.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Cntysoft Cloud Software Team
+ * 
+ * @author wql <wql1211608804@163.com>
+ * @copyright Copyright (c) 2010-2011 Cntysoft Technologies China Inc. <http://www.cntysoft.com>
+ * @license http://www.cntysoft.com/license/new-bsd     New BSD License
+ */
+namespace Cntysoft\Framework\Cloud\Ali\Push;
+class ResultSet
+{
+   /**
+    * 返回的错误码
+    * */
+   public $code;
+   /**
+    * 返回的错误信息
+    * */
+   public $msg;
+
+}
